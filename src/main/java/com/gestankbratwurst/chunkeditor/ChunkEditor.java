6d78da1e8071e892b34162f9aa8e4ce8fc4ffc6f
Flo0 <unconfigured@null.spigotmc.org>
2020-07-25 16:09:36 +0200

    Spaced main class

--- src/main/java/com/gestankbratwurst/chunkeditor/ChunkEditor.java
+++ src/main/java/com/gestankbratwurst/chunkeditor/ChunkEditor.java
@@ -1,7 +1,6 @@
 package com.gestankbratwurst.chunkeditor;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /*******************************************************
